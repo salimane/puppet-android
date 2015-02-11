d7f3557d844d83493b543c51a3582a9c585cf82e
Makoto Takezawa <makoto@takezawa.biz>
2015-02-11 15:28:36 +0900

    Update Android Studio to 1.0.1.

--- manifests/studio.pp
+++ manifests/studio.pp
@@ -4,10 +4,15 @@
 # Usage:
 #
 #     include android::studio
-class android::studio($release = '0.4.6', $version = '133.1028713') {
+class android::studio($release = '1.0.1', $version = '1641136') {
 
-  package { 'Android Studio':
+  case $release[0] {
+    '0' : { $_source = "http://dl.google.com/android/studio/install/${release}/android-studio-bundle-${version}-mac.dmg" }
+    '1' : { $_source = "https://dl.google.com/dl/android/studio/install/${release}/android-studio-ide-${version}.dmg" }
+  }
+
+  package { 'Android Studio-{$release}-{$version}':
     provider => 'appdmg',
-    source   => "http://dl.google.com/android/studio/install/${release}/android-studio-bundle-${version}-mac.dmg"
+    source   => $_source
   }
 }
